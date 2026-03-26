1d9e3ab107c0f669cf2b2c95983a41a6f47effc4
KANISHGA <168720129+kanishgask@users.noreply.github.com>
2026-03-26 17:25:44 +0530

    Create events.js

---
+++ events.js
@@ -0,0 +1,6 @@
+function deleteTask(index) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    location.reload();
+}
